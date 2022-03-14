333b7e0fb1dcda73d5c6d5af600d4bad6f3459a6
Hùng Nguyễn <hungduy3n2k1@gmail.com>
2022-03-14 04:52:57 +0000

    Update views/define_request/index.php

--- views/define_request/index.php
+++ views/define_request/index.php
@@ -8,9 +8,11 @@
             <section class="app-user-list">
                 <!-- list section start -->
                 <div class="card">
-                <h2 class="content-header-title float-left mb-0" id="title_module" style="border-right-width: 0px;">
-                       Cài đặt yêu cầu <img src="<?= HOME ?>/layouts/tooltip.png" style="margin: 0 15px" width="25px" id="current_ip" data-toggle="tooltip" data-placement="right" data-original-title="Là chức năng ..." data-trigger="click">
-                    </h2>
+                <div class="d-flex justify-content-between align-items-center mx-50 row pt-2 pb-2">
+                        <h2 class="content-header-title float-left mb-0" id="title_module" style="border-right-width: 0px;">
+                            Cài đặt yêu cầu <img src="<?= HOME ?>/layouts/tooltip.png" style="margin: 0 15px" width="25px" id="current_ip" data-toggle="tooltip" data-placement="right" data-original-title="Là chức năng ..." data-trigger="click">
+                        </h2>
+                    </div>
                     <div class="card-datatable table-responsive pt-0">
                         <table class="user-list-table table">
                             <thead class="thead-light">
