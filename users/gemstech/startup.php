7d9699db44445f16f94a0477eacdbba9e9ec7212
hungduy3n2k1 <hungduy3n2k1@gmail.com>
2022-03-23 09:40:57 +0700

    test firebase

--- users/gemstech/startup.php
+++ users/gemstech/startup.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_TYPE", "mysql");
 define("DB_HOST", "localhost");
-define("DB_NAME", "goffice_gemstechnew");
+define("DB_NAME", "goffice_gemstech");
 define("DB_USER", "root");
 define("DB_PASS", "");
 //define("URL", "http://".$_SERVER["HTTP_HOST"]."/gofficev2/api");
